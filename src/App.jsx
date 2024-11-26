43f1298ebc138595a9d78ab554baa66fdf749d51
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-26 14:40:38 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Control from './components/control'
 import Timer from './components/Timer';
 import './App.css'
@@ -14,15 +14,50 @@ function App() {
   const defaultSession = 25;
   const defaultPhase = phase.session;
 
-  const [breakLength, setBreakLength] = useState(defaultBreak);
-  const [sessionLength, setSessionLength] = useState(defaultSession);
+  const [breakLength, setBreakLength] = useState(1);
+  const [sessionLength, setSessionLength] = useState(1);
   const [currentPhase, setCurrentPhase] = useState(defaultPhase);
+  const [currentTime, setCurrentTime] = useState(sessionLength * 60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCurrentTime(prevTime => {
+          console.log(prevTime);
+
+          if (prevTime <= 0) {
+            clearInterval(interval);
+            return 0;
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const toggleRunnning = () => {
+    setIsRunning(!isRunning);
+  }
+
+  const reset = () => {
+    setCurrentPhase(phase.session);
+    setBreakLength(defaultBreak);
+    setSessionLength(defaultSession);
+    setCurrentTime(0);
+  }
 
   return (
     <div>
       <Control controlName={"break"} defaultLength={breakLength}></Control>
       <Control controlName={"session"} defaultLength={sessionLength}></Control>
-      <Timer currentPhase={currentPhase}></Timer>
+      <Timer currentPhase={currentPhase} timeLeft={currentTime} resetOnClick={reset}></Timer>
     </div>
   )
 }
