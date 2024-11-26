ac1fa41c91ebb5d6589f005988db8c378dbbd3e0
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-26 13:28:39 +0800

    Update Control.jsx

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -1,5 +1,16 @@
-function Control(){
-
+function Control({ controlName,defaultLength }) {
+    return (
+        <div className={controlName}>
+            <div>
+                <p id={controlName + "-label"}>{controlName.toUpperCase()} Length</p>
+            </div>
+            <div>
+                <div id={controlName+"-decrement"}></div>
+                <div id={controlName+"-length"}></div>
+                <div id={controlName+"-increment"}></div>
+            </div>
+        </div>
+    );
 }
 
 export default Control;
