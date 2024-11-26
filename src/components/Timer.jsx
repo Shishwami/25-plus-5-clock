7f976bfe38b6b2ff5a83cc092e5144fe70806c68
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-26 14:40:42 +0800

    Update Timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,17 @@
-function Timer({currentPhase,timeLeft}) {
+function Timer({ currentPhase, timeLeft, resetOnClick, startStopOnClick }) {
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const secondsRemaining = seconds % 60;
+        return `${minutes < 10 ? '0' : ''}${minutes}:${secondsRemaining < 10 ? '0' : ''}${secondsRemaining}`;
+    };
 
     return (
         <div className="timer">
             <p id="timer-label">{currentPhase}</p>
-            <p id="time-left">{timeLeft}</p>
-            <button id="start_stop"></button>
-            <button id="reset"></button>
+            <p id="time-left">{formatTime(timeLeft)}</p>
+            <button id="start_stop" onClick={startStopOnClick}></button>
+            <button id="reset" onClick={resetOnClick}></button>
         </div>
     )
 }
