3cc946dfed487f18a0b568bbf868dc7f4506f00e
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-26 13:49:55 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
 import Control from './components/control'
+import Timer from './components/Timer';
 import './App.css'
 
 function App() {
 
-  const defaultBreak=5;
-  const defaultSession=25;
+  const phase = {
+    session: "Session",
+    break: "Break"
+  };
+
+  const defaultBreak = 5;
+  const defaultSession = 25;
+  const defaultPhase = phase.session;
+
+  const [breakLength, setBreakLength] = useState(defaultBreak);
+  const [sessionLength, setSessionLength] = useState(defaultSession);
+  const [currentPhase, setCurrentPhase] = useState(defaultPhase);
 
   return (
     <div>
-      <Control controlName={"break"} defaultLength={defaultBreak}></Control>
-      <Control controlName={"session"} defaultLength={defaultSession}></Control>
+      <Control controlName={"break"} defaultLength={breakLength}></Control>
+      <Control controlName={"session"} defaultLength={sessionLength}></Control>
+      <Timer currentPhase={currentPhase}></Timer>
     </div>
   )
 }
